f89ab4e5cb11968242166370f552e661b142cb37
Bartosz <krawiecbartosz55@gmail.com>
2021-01-20 16:00:24 +0100

    created core project functionality

--- Projekt/fuel_prices.py
+++ Projekt/fuel_prices.py
@@ -1,40 +1,87 @@
 from Projekt.fuels import *
 from bs4 import BeautifulSoup
-import requests
+from requests import get as http_get
 
-page = requests.get("https://www.orlen.pl/PL/DlaBiznesu/HurtoweCenyPaliw/Strony/default.aspx")
+URL = "https://www.orlen.pl/PL/DlaBiznesu/HurtoweCenyPaliw/Strony/default.aspx"
 
 
-def parse_float(number):
-    number = number[0] + number[2:5]
-    return float(number) / 1000
+def get_page(url):
+    page = http_get(url)
+    if page.status_code != 200:
+        print("Nie można pobrać strony! Błąd ", page.status_code)
+        exit(1)
+    else:
+        return page
+
+
+def parse_float_fuel_price(number):
+    try:
+        number = number[0] + number[2:5]
+        return float(number) / 1000
+
+    except ValueError as e:
+        print("Błąd przy konwertowaniu ceny: ", e)
+        return 0
+
+
+def add_vat(price):
+    return round(price / 0.78, 2)
 
 
 def get_fuels_table(page):
     soup = BeautifulSoup(page.content, "html.parser")
-    return soup.find_all("table", class_=["tableLight", "orlen-styled-table"])[0]
+    return soup.find("table", class_=["tableLight", "orlen-styled-table"])
 
 
-def get_fuel_table_rows(fuel_table):
+def get_table_rows(fuel_table):
     return fuel_table.find_all("tr")[1:]
 
 
-def get_fuel_data(row):
+def get_fuel_data_from_row(row):
     data = row.text.strip().splitlines()
-    data[1] = parse_float(data[1])
-    return data
+    price = parse_float_fuel_price(data[1])
+    fuel = Fuel(data[0], add_vat(price))
+    return fuel
 
 
-def get_fuels():
-    fuel_table = get_fuels_table(page)
-    fuel_table_rows = get_fuel_table_rows(fuel_table)
-
+def get_fuels_data(rows):
     fuels = []
-    for row in fuel_table_rows:
-        fuel_data = get_fuel_data(row)
-        fuels.append(Fuel(fuel_data[0], fuel_data[1]))
+    for row in rows:
+        fuel_data = get_fuel_data_from_row(row)
+        fuels.append(fuel_data)
 
     return fuels
 
 
-print(get_fuels())
+def get_fuels():
+    page = get_page(URL)
+    fuel_table = get_fuels_table(page)
+    rows = get_table_rows(fuel_table)
+    fuels = get_fuels_data(rows)
+
+    return fuels
+
+
+def get_date():
+    page = get_page(URL)
+    soup = BeautifulSoup(page.content, "html.parser")
+    tables = soup.findAll("table",
+                          {
+                            "class": "s4-wpTopTable",
+                            "border": "0",
+                            "cellpadding": "0",
+                            "width": "100%"
+                          })
+    date_paragraph = tables[1].findChildren("p")[0]
+    spans = date_paragraph.findChildren("span")
+    return spans[0].text
+
+
+if __name__ == '__main__':
+    date = get_date()
+    fuels = get_fuels()
+
+    print("\n\nCeny paliw z dnia {}\n".format(date))
+
+    for fuel in fuels:
+        print(fuel)
--- Projekt/fuels.py
+++ Projekt/fuels.py
@@ -1,5 +1,5 @@
 import enum
-import date
+from datetime import datetime
 
 class FuelType(enum.Enum):
     petrol = "Benzyna"
@@ -9,12 +9,14 @@ class FuelType(enum.Enum):
 
 class Fuel:
 
-    def __init__(self, type=FuelType.petrol, price=0):
-        self.type = type
+    def __init__(self, name=FuelType.petrol, price=0):
+        self.name = name
         self.price = price
-        self.date = date.today()
+        self.date = datetime.date(datetime.now())
+
     def __str__(self):
-        return "{0} - {1}".format(self.type, self.price)
+        return "{0} - {1} zł/l".format(self.name, self.price)
 
     def __repr__(self):
-        return "{0} - {1}".format(self.type, self.price)
+        return "{0} - {1} zł/l".format(self.name, self.price)
+
